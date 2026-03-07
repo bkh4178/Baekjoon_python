35a52b648e60ede41fb40b467ac1d7aa3cf9a0c0
Geonho <bkh4178@gmail.com>
2026-03-07 16:41:48 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 5324 ms, Memory: 294968 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,49 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+def heapq_solver():
+    T = int(input())
+    for _ in range(T):
+        k = int(input())
+        min_heap = []
+        max_heap = []
+        visited = [False] * k 
+        
+        for i in range(k):
+            count = input().split()
+            option, num = count[0], int(count[1])
+            
+            if option == 'I':
+                heapq.heappush(min_heap, (num, i))
+                heapq.heappush(max_heap, (-num, i))
+                visited[i] = True
+                
+            elif option == 'D':
+                if num == -1:
+                    while min_heap and not visited[min_heap[0][1]]:
+                        heapq.heappop(min_heap)
+                    
+                    if min_heap:
+                        visited[min_heap[0][1]] = False
+                        heapq.heappop(min_heap)
+                        
+                elif num == 1:
+                    while max_heap and not visited[max_heap[0][1]]:
+                        heapq.heappop(max_heap)
+                        
+                    if max_heap:
+                        visited[max_heap[0][1]] = False
+                        heapq.heappop(max_heap)
+        
+        while min_heap and not visited[min_heap[0][1]]:
+            heapq.heappop(min_heap)
+        while max_heap and not visited[max_heap[0][1]]:
+            heapq.heappop(max_heap)
+            
+        if not min_heap:
+            print("EMPTY")
+        else:
+            print(-max_heap[0][0], min_heap[0][0])
+
+heapq_solver()
