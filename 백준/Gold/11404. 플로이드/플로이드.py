425668c3e1ab79c8e92d31f9ea9c6f7ea18a7016
Geonho <bkh4178@gmail.com>
2026-03-21 14:14:34 +0900

    [Gold IV] Title: 플로이드, Time: 452 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -7,9 +7,7 @@ n, m = int(input()), int(input())
 graph = [[INF]*(n+1) for _ in range(n+1)]
 
 for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i == j:
-            graph[i][j] = 0
+    graph[i][i] = 0
 
 for _ in range(m):
     a, b, w = map(int, input().split())
