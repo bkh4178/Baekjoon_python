26e8012860d569a53f1e467c57b5a74deb5959ac
Geonho <bkh4178@gmail.com>
2026-03-28 10:20:39 +0900

    [Gold IV] Title: 운동, Time: 6064 ms, Memory: 37660 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,21 +1,31 @@
 import sys
 input = sys.stdin.readline
-INF = int(1e10)
-V, E = map(int, input().split())
-graph = [[INF]*(V+1) for _ in range(V+1)]
-for _ in range(E):
-    a, b, c = map(int, input().split())
-    graph[a][b] = c
 
-min_cy = INF
-for k in range(1, V+1):
-    for i in range(1, V+1):
-        if graph[i][k] != INF:
-            for j in range(1, V+1):
-                if graph[k][j] != INF:
-                    cost = graph[i][k]+graph[k][j]
-                    if cost < graph[i][j]:
-                        graph[i][j] = cost
+INF = 10**15
 
-min_cy = min([graph[i][i] for i in range(1, V+1)])
-print(-1 if min_cy == INF else min_cy)
+def floyd(dist, V):
+    for k in range(1, V + 1):
+        for i in range(1, V + 1):
+            for j in range(1, V + 1):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+def main():
+    V, E = map(int, input().split())
+
+    dist = [[INF] * (V + 1) for _ in range(V + 1)]
+
+    for _ in range(E):
+        a, b, c = map(int, input().split())
+        dist[a][b] = c
+
+    floyd(dist, V)
+
+    answer = INF
+    for i in range(1, V + 1):
+        answer = min(answer, dist[i][i])
+
+    print(-1 if answer == INF else answer)
+
+if __name__ == "__main__":
+    main()
